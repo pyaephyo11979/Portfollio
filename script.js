268b0832168f1ce4d48490c4d7b9ebe86248b841
pyaephyo11979 <www.pyaephyoehlaing16@gmail.com>
2023-07-19 19:26:39 +0630

    Add last updates

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 let container=document.querySelector('body');
-let  button=document.getElementById('dNBtn')
+let  button=document.getElementById('dNBtn');
+let navItem=document.getElementById("navItem");
 function dark(){
     container.setAttribute('data-bs-theme','dark');
-    button.style.backgroundColor="white";
-    button.style.color="black";
+    button.style.color="white";
 }
 function light(){
     container.setAttribute('data-bs-theme','light');
-    button.style.backgroundColor="black";
-    button.style.color="white";
+    button.style.color="black";
 }
 button.addEventListener('click',()=>{
     if(!container.hasAttribute('data-bs-theme')){
