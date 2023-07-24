45ed498797ba043c9a2d19bb4359b4c5a99e2f77
pyaephyo11979 <www.pyaephyoehlaing16@gmail.com>
2023-07-24 15:06:12 +0630

    Updates

--- Projects/Sample Shop Project1/script.js
+++ Projects/Sample Shop Project1/script.js
@@ -1,22 +1,30 @@
 let cartbadge=document.getElementById("cart_item")
 let btn=document.getElementById("btn");
 let badge=0;
-let menClothings=document.getElementById("mencl");
-let womenClothings=document.getElementById("womcl");
+let menClothings=document.getElementById("men's clothing");
+let womenClothings=document.getElementById("women's clothng");
 let usualProducts=document.getElementById("aPProducts")
-
-// fetch('https://fakestoreapi.com/products')
-// .then(res=>{
-//    return res.json();
-// })
-// .then(data =>{
-//     conaole.log(data);
-// })
-// btn.addEventListener('click',function(){ 
-   
-//     badge+=1;
-//     cartbadge.innerHTML=`${badge}`
-// })
+let products=document.querySelector('#productDisplay');
+product.forEach((val)=>{
+    console.log(val)
+    console.log(products)
+    products.innerHTML +=`
+    <div class="col mt-2 mb-2"  id="${val.category}">
+    <div class="card" style="width:300px; height:420px;">
+        <div class="card-body">
+            <div class="card-title">${val.title}</div>
+            <div class="card-img"><img src="${val.image}" class="img-fluid" style="width:50%" alt="AirJordan"></div>
+            <p class="mt-auto"><b>$ ${val.price}</b></p>
+        </div>
+        <div class="card-footer">
+            <button  type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#productinfo">BuyNow <i class="fas fa-credit-card"></i> </button>
+            <button type="button" id="btn" onclick="addtocart()" class="btn btn-secondary">AddToCart <i class="fas fa-shopping-cart"></i></button>
+        </div>
+    </div>
+</div>
+    ` 
+    console.log(val.category);
+})
 function men(){
     usualProducts.style.display="none";
     womenClothings.style.display="none";
